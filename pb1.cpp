47102a11d8fac7b9620fcaf1a429aae263ad1110
youef0x <157964766+youef0x@users.noreply.github.com>
2024-01-28 15:09:01 +0200

    Add files via upload
    * Description 	        :    Sum even terms less than 4e6 of Fibbonaci sequence 
    * Link 			:    https://projecteuler.net/problem=2
    * Language		:    C++

---
+++ pb1.cpp
@@ -0,0 +1,37 @@
+/**
+* Description 	: Sum even terms less than 4e6 of Fibbonaci sequence 
+* Link 			: https://projecteuler.net/problem=2
+* Language		: C++
+*/
+
+#include <iostream>
+
+
+int main() {
+
+
+	int x   = 1; 
+	int y   = 2;
+	int z   = x + y;
+	int sum = 2;
+	
+	while(1)
+	{
+
+		x  = y;
+		y  = z;
+		z  = x + y;
+		
+		if(y%2 == 0) sum+=y;
+						
+		
+		if(z > 4e6) break;
+		
+	}
+	
+	
+	std::cout << sum ;
+	
+	
+
+}
